f61f5180dd9595b3bd59fa09c1569598a0044b84
Do Van Phu <Do Van Phu@DESKTOP-PUCRF7R>
2024-02-15 18:59:03 +0700

    updated

--- TestSAMServer/Form1.Designer.cs
+++ TestSAMServer/Form1.Designer.cs
@@ -26,12 +26,12 @@
             this.components = new System.ComponentModel.Container();
             this.iv = new LotusAPI.Controls.FastImageView();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.bt_CONNECT = new Abeo.Controls.Roundable.RoundButton();
             this.flatLabel1 = new Abeo.Controls.FlatLabel();
             this.flatPanel1 = new Abeo.Controls.FlatPanel();
             this.bt_LOAD = new Abeo.Controls.Roundable.RoundButton();
             this.tb_IP = new Abeo.Controls.Roundable.RoundTextbox();
             this.logView1 = new LotusAPI.Controls.LogView();
+            this.ckb_InROI = new Abeo.Controls.FlatCheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.iv)).BeginInit();
             this.flatPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -57,56 +57,15 @@
             this.iv.TabIndex = 1;
             this.iv.UseCustomMouseEvent = false;
             this.iv.ZoomPanModifierKey = System.Windows.Forms.Keys.None;
+            this.iv.MouseClick += new LotusAPI.Controls.FastImageView.MouseEventHandler(this.iv_MouseClick);
+            this.iv.PostRenderDrawEvent += new LotusAPI.Controls.FastImageView.PostRenderDrawEventHandler(this.iv_PostRenderDrawEvent);
             this.iv.ROISelectedEvent += new LotusAPI.Controls.FastImageView.ROISelectedEventHandler(this.iv_ROISelectedEvent);
+            this.iv.KeyUp += new System.Windows.Forms.KeyEventHandler(this.iv_KeyUp);
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
-            // 
-            // bt_CONNECT
-            // 
-            this.bt_CONNECT.BackColor = System.Drawing.Color.Transparent;
-            this.bt_CONNECT.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(110)))), ((int)(((byte)(118)))));
-            this.bt_CONNECT.BorderCorners = Abeo.Controls.Roundable.Corners.All;
-            this.bt_CONNECT.BorderRadius = 3;
-            this.bt_CONNECT.BorderThickness = 0;
-            this.bt_CONNECT.Checked = false;
-            this.bt_CONNECT.CheckEnable = false;
-            this.bt_CONNECT.ContentBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(44)))), ((int)(((byte)(55)))));
-            this.bt_CONNECT.ContentBackColorChecked = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(77)))), ((int)(((byte)(88)))));
-            this.bt_CONNECT.ContentBackColorMouseDown = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(44)))), ((int)(((byte)(55)))));
-            this.bt_CONNECT.ContentBackColorMouseOver = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(99)))), ((int)(((byte)(110)))));
-            this.bt_CONNECT.ControlToBringToFront = null;
-            this.bt_CONNECT.DebugMode = false;
-            this.bt_CONNECT.Dock = System.Windows.Forms.DockStyle.Left;
-            this.bt_CONNECT.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.bt_CONNECT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(161)))), ((int)(((byte)(161)))));
-            this.bt_CONNECT.ForeColorChecked = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(44)))), ((int)(((byte)(55)))));
-            this.bt_CONNECT.ForeColorMouseDown = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(161)))), ((int)(((byte)(161)))));
-            this.bt_CONNECT.ForeColorMouseOver = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(161)))), ((int)(((byte)(161)))));
-            this.bt_CONNECT.Icon = null;
-            this.bt_CONNECT.IconPortion = 0.3F;
-            this.bt_CONNECT.IconSize = 24;
-            this.bt_CONNECT.IconVisible = false;
-            this.bt_CONNECT.Image = null;
-            this.bt_CONNECT.ImageSizeMode = Abeo.Controls.Roundable.SizeMode.Stretch;
-            this.bt_CONNECT.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
-            this.bt_CONNECT.IsExclusive = false;
-            this.bt_CONNECT.Location = new System.Drawing.Point(225, 3);
-            this.bt_CONNECT.Name = "bt_CONNECT";
-            this.bt_CONNECT.Size = new System.Drawing.Size(109, 32);
-            this.bt_CONNECT.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            this.bt_CONNECT.TabIndex = 3;
-            this.bt_CONNECT.TagColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(66)))), ((int)(((byte)(129)))));
-            this.bt_CONNECT.TagLocation = Abeo.Controls.Roundable.TagLocation.Left;
-            this.bt_CONNECT.TagOffset = 5;
-            this.bt_CONNECT.TagVisible = false;
-            this.bt_CONNECT.TagWidth = 5;
-            this.bt_CONNECT.Text = "CONNECT";
-            this.bt_CONNECT.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
-            this.bt_CONNECT.TextIconRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.bt_CONNECT.Click += new System.EventHandler(this.bt_CONNECT_Click);
             // 
             // flatLabel1
             // 
@@ -125,8 +84,8 @@
             // flatPanel1
             // 
             this.flatPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.flatPanel1.Controls.Add(this.ckb_InROI);
             this.flatPanel1.Controls.Add(this.bt_LOAD);
-            this.flatPanel1.Controls.Add(this.bt_CONNECT);
             this.flatPanel1.Controls.Add(this.tb_IP);
             this.flatPanel1.Controls.Add(this.flatLabel1);
             this.flatPanel1.Dock = System.Windows.Forms.DockStyle.Top;
@@ -167,7 +126,7 @@
             this.bt_LOAD.ImageSizeMode = Abeo.Controls.Roundable.SizeMode.Stretch;
             this.bt_LOAD.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
             this.bt_LOAD.IsExclusive = false;
-            this.bt_LOAD.Location = new System.Drawing.Point(334, 3);
+            this.bt_LOAD.Location = new System.Drawing.Point(225, 3);
             this.bt_LOAD.Name = "bt_LOAD";
             this.bt_LOAD.Size = new System.Drawing.Size(141, 32);
             this.bt_LOAD.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
@@ -252,6 +211,26 @@
             this.logView1.Text = "";
             this.logView1.UpdateInterval = 100;
             // 
+            // ckb_InROI
+            // 
+            this.ckb_InROI.BackColor = System.Drawing.Color.Transparent;
+            this.ckb_InROI.Checked = false;
+            this.ckb_InROI.ColorOFF = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(54)))), ((int)(((byte)(66)))));
+            this.ckb_InROI.ColorON = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(150)))), ((int)(((byte)(0)))));
+            this.ckb_InROI.ColorSwitch = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(231)))), ((int)(((byte)(212)))));
+            this.ckb_InROI.ColorSwitchText = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(231)))), ((int)(((byte)(212)))));
+            this.ckb_InROI.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ckb_InROI.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(231)))), ((int)(((byte)(212)))));
+            this.ckb_InROI.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+            this.ckb_InROI.Location = new System.Drawing.Point(396, 3);
+            this.ckb_InROI.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ckb_InROI.Name = "ckb_InROI";
+            this.ckb_InROI.Size = new System.Drawing.Size(127, 32);
+            this.ckb_InROI.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.ckb_InROI.SwitchHeight = 16;
+            this.ckb_InROI.TabIndex = 7;
+            this.ckb_InROI.Text = "Find in ROI";
+            // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
@@ -272,12 +251,12 @@
 
         private LotusAPI.Controls.FastImageView iv;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private Abeo.Controls.Roundable.RoundButton bt_CONNECT;
         private Abeo.Controls.FlatLabel flatLabel1;
         private Abeo.Controls.FlatPanel flatPanel1;
         private LotusAPI.Controls.LogView logView1;
         private Abeo.Controls.Roundable.RoundTextbox tb_IP;
         private Abeo.Controls.Roundable.RoundButton bt_LOAD;
+        private Abeo.Controls.FlatCheckBox ckb_InROI;
     }
 }
 
--- TestSAMServer/Form1.cs
+++ TestSAMServer/Form1.cs
@@ -13,6 +13,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Img = LotusAPI.MV.Image;
 
 namespace TestSAMServer {
     public partial class Form1 : Form {
@@ -24,62 +25,104 @@ namespace TestSAMServer {
             Library.Initialize();
         }
 
-        private void bt_CONNECT_Click(object sender, EventArgs e) {
-            try {
-                _client.Disconnect();
-                _client = new SimpleTcpClient();
-                _client.Connect(tb_IP.Text, 8727, 10000, 10000);
-                if(_client.Connected)
-                    Logger.Info("CONNECTED");
-                else
-                    throw new Exception("Failed to connect!");
-            } catch(Exception ex) { Logger.Error(ex.Message); Logger.Trace(ex.StackTrace); }
+        void Connect() {
+            _client.Disconnect();
+            _client = new SimpleTcpClient();
+            _client.Connect(tb_IP.Text, 8727, 10000, 10000);
+            if (_client.Connected)
+                Logger.Info("CONNECTED");
+            else
+                throw new Exception("Failed to connect!");
+        }
+        void Disconnect() {
+            _client?.Disconnect();
         }
 
         LotusAPI.MV.Image _img;
-        byte[] _imgbuf;
+
+        String GetJsonStr(Json j) => j.ToString().Replace("\n", "").Replace(" ", "") + "\r\n";
+        void SendRequest(Json request) {
+            if (!_client.Connected) throw new Exception("Client not connected!");
+            var str = GetJsonStr(request);
+            var buf = ASCIIEncoding.ASCII.GetBytes(str);
+            _client.Send(buf, 0, buf.Length);
+            var res = _client.ReadLine(1000);//expect OK 
+            if (res != "OK") {
+                throw new Exception($"Failed to send request (RES={res})");
+            }
+            Logger.Debug(">" + res);
+        }
+
+        void SetImage(Img img) {
+            //convert image to PNG buffer
+            byte[] imgbuf = img.EncodePNG();
+            Json request = new Json();
+            request["name"] = "set_img";
+            request["size"] = imgbuf.Length;
+            SendRequest(request);
+            _client.Send(imgbuf, 0, imgbuf.Length);
+            var res = _client.ReadLine(100000);//expect OK 
+            if (res != "OK") {
+                throw new Exception($"Failed to send request (RES={res})");
+            }
+            Logger.Debug(">" + res);
+        }
+
         private void bt_LOAD_Click(object sender, EventArgs e) {
             try {
                 _img = DialogUtils.OpenImage("Open image").ToBGR();
-                _imgbuf = _img.EncodePNG();
                 iv.SetImage(_img);
+                Connect();
+                SetImage(_img);
                 iv.AutoScale();
-            } catch(Exception ex) { Logger.Error(ex.Message); Logger.Trace(ex.StackTrace); }
+            }
+            catch (Exception ex) { Logger.Error(ex.Message); Logger.Trace(ex.StackTrace); }
+            Disconnect();
         }
 
+        List<Point2i> _pnts = new List<Point2i>();
         private void iv_ROISelectedEvent(RectangleF roi) {
+            if(ckb_InROI.Checked) Predict(roi, _pnts);
+        }
+        private void Predict(RectangleF roi, List<Point2i> pnts) {
             try {
-                if(_img == null) throw new Exception("No image");
+                if (_img == null) throw new Exception("No image");
                 //create request message
-                Json j = new Json();
-                j["name"] = "predict";
-                j["size"] = _imgbuf.Length;
+                Json request = new Json();
+                request["name"] = "predict";
                 var rect = (Rect2i)roi;
-                j["box"][0] = rect.X;
-                j["box"][1] = rect.Y;
-                j["box"][2] = rect.X + rect.Width;
-                j["box"][3] = rect.Y + rect.Height;
-                var cen = (rect.TopLeft + rect.BottomRight) / 2;
-                cen.Write(j["point"][0]);
-                j["label"][0] = 0;
-                j["multimask"] = true;
-                var str = j.ToString().Replace("\n", "").Replace(" ", "") + "\r\n";
+                request["box"][0] = rect.X;
+                request["box"][1] = rect.Y;
+                request["box"][2] = rect.X + rect.Width;
+                request["box"][3] = rect.Y + rect.Height;
+                if (pnts == null) {
+                    var cen = (rect.TopLeft + rect.BottomRight) / 2;
+                    cen.Write(request["point"][0]);
+                    request["label"][0] = 1;
+                }
+                else {
+                    for (int i = 0; i < pnts.Count; i++) {
+                        pnts[i].Write(request["point"][i]);
+                        request["label"][i] = 1;
+                    }
+                }
+
+                request["multimask"] = true;
+                var str = request.ToString().Replace("\n", "").Replace(" ", "") + "\r\n";
                 Logger.Debug(str);
 
-                if(!_client.Connected) throw new Exception("Client not connected!");
+                Connect();
+
                 //send request
                 var buf = ASCIIEncoding.ASCII.GetBytes(str);
                 _client.Send(buf, 0, buf.Length);
                 var res = _client.ReadLine(1000);//expect OK 
-                if(res != "OK") {
+                if (res != "OK") {
                     throw new Exception($"Failed to send request (RES={res})");
                 }
                 Logger.Debug(">" + res);
-                //send image data
-                _client.Send(_imgbuf, 0, _imgbuf.Length);
-                res = _client.ReadLine(10000);//expect OK 
-                Logger.Debug(">" + res);
 
+                res = _client.ReadLine(100000);//expect OK 
                 //parse response
                 Json jres = Json.FromString(res);
                 int mask_id = jres["mask"];
@@ -87,14 +130,13 @@ namespace TestSAMServer {
                 int mask_size = jres["size"];
                 byte[] res_buf = new byte[mask_size];
                 _client.Read(res_buf, 0, mask_size);
-                res = _client.ReadLine(10000);//expect OK 
-                if(res != "OK") {
+                res = _client.ReadLine(1000);//expect OK 
+                if (res != "OK") {
                     throw new Exception($"Failed to receive result data (RES={res})");
                 }
                 Logger.Debug(">" + res);
                 //read mask data
                 var mask_img = LotusAPI.MV.Image.Decode(res_buf, LotusAPI.MV.ImageDecodeOption.Grayscale).ToGray();
-
 
                 //create overlay
                 var img = _img.Clone() as LotusAPI.MV.Image;
@@ -104,9 +146,34 @@ namespace TestSAMServer {
                 //draw outline
                 mask_img.FindContours(20)
                     .ToList()
-                    .ForEach(x => img.DrawPoly(x, Color.Magenta, 2,true));
+                    .ForEach(x => img.DrawPoly(x, Color.Magenta, 2, true));
                 iv.SetImage(img);
-            } catch(Exception ex) { Logger.Error(ex.Message); Logger.Trace(ex.StackTrace); }
+            }
+            catch (Exception ex) { Logger.Error(ex.Message); Logger.Trace(ex.StackTrace); }
+            Disconnect();
+        }
+
+
+
+        private void iv_MouseClick(FastImageView sender, FastImageView.MouseEventArgs e) {
+            if(ModifierKeys == Keys.Control) {
+                _pnts.Add(e.Location);
+                if(!ckb_InROI.Checked) Predict(new RectangleF(), _pnts);
+                iv.Invalidate();
+            }
+        }
+
+        private void iv_PostRenderDrawEvent(FastImageView c) {
+            try { 
+                for(int i = 0; i < _pnts.Count; i++) {
+                    c.DrawCircle(_pnts[i], 3, Color.Lime, -1);
+                }
+            }catch { }
+        }
+
+        private void iv_KeyUp(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Escape) _pnts.Clear();
+            iv.Invalidate();
         }
     }
 }
